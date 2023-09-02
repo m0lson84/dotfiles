5e1752cd8dad27d35de87f4f3827af11cc30ca30
Michael Olson <michaelolson84@gmail.com>
2023-09-02 11:37:59 -0400

    Simplifying language configuration in Neovim

--- home/private_dot_config/nvim/lua/plugins/lang/init.lua
+++ home/private_dot_config/nvim/lua/plugins/lang/init.lua
@@ -4,12 +4,12 @@ Additional language server protocol (LSP) plugins
 
 return {
   { import = 'lazyvim.plugins.extras.lang.docker' },
+  { import = 'lazyvim.plugins.extras.lang.go' },
   { import = 'lazyvim.plugins.extras.lang.json' },
+  { import = 'lazyvim.plugins.extras.lang.rust' },
   { import = 'lazyvim.plugins.extras.lang.yaml' },
   require('plugins.lang.bash'),
-  require('plugins.lang.go'),
   require('plugins.lang.markdown'),
   require('plugins.lang.python'),
-  require('plugins.lang.rust'),
   require('plugins.lang.typescript'),
 }
