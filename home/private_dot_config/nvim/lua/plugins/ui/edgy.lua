0abb5d9c33a83e0654b6572d83f21bb368fb0252
Michael Olson <michaelolson84@gmail.com>
2023-09-09 11:14:43 -0400

    Update edgy layouts

--- home/private_dot_config/nvim/lua/plugins/ui/edgy.lua
+++ home/private_dot_config/nvim/lua/plugins/ui/edgy.lua
@@ -2,40 +2,39 @@
 edgy.nvim (https://github.com/folke/edgy.nvim)
 --]]
 
+local panel_height = 0.25
+local sidebar_width = 0.2
+
 return {
   { import = 'lazyvim.plugins.extras.ui.edgy' },
   {
     'folke/edgy.nvim',
     opts = function()
       local opts = {
-        animate = {
-          enabled = false,
-        },
+        animate = { enabled = false },
         bottom = {
-          'Trouble',
           {
             title = 'Terminal',
             ft = 'lazyterm',
-            size = { height = 0.2 },
+            size = { height = panel_height },
             filter = function(buf) return not vim.b[buf].lazyterm_cmd end,
           },
           {
             title = 'Notifications',
             ft = 'noice',
-            size = { height = 0.2 },
+            size = { height = panel_height },
             filter = function(_, win) return vim.api.nvim_win_get_config(win).relative == '' end,
           },
           {
             title = 'Help',
             ft = 'help',
-            size = { height = 0.2 },
+            size = { height = panel_height },
             filter = function(buf) return vim.bo[buf].buftype == 'help' end,
           },
-          {
-            title = 'Test Output',
-            ft = 'neotest-output-panel',
-            size = { height = 0.2 },
-          },
+          'Trouble',
+          { title = 'QuickFix', ft = 'qf' },
+          { title = 'Spectre', ft = 'spectre_panel', size = { height = panel_height } },
+          { title = 'Test Output', ft = 'neotest-output-panel', size = { height = panel_height } },
         },
         left = {
           {
@@ -44,26 +43,23 @@ return {
             filter = function(buf) return vim.b[buf].neo_tree_source == 'filesystem' end,
             open = function() vim.api.nvim_input('<esc><space>e') end,
             pinned = true,
-            size = { height = 0.5, width = 0.2 },
+            size = { height = 0.5, width = sidebar_width },
           },
           {
             title = 'Git',
             ft = 'neo-tree',
             filter = function(buf) return vim.b[buf].neo_tree_source == 'git_status' end,
             open = 'Neotree position=right git_status',
-            size = { width = 0.2 },
+            size = { width = sidebar_width },
           },
           {
             title = 'Buffers',
             ft = 'neo-tree',
             filter = function(buf) return vim.b[buf].neo_tree_source == 'buffers' end,
             open = 'Neotree position=top buffers',
-            size = { width = 0.2 },
+            size = { width = sidebar_width },
           },
-          {
-            title = 'Test Summary',
-            ft = 'neotest-summary',
-          },
+          { title = 'Test Summary', ft = 'neotest-summary', size = { width = sidebar_width } },
           'neo-tree',
         },
         keys = {
--- home/private_dot_config/wezterm/font.lua
+++ home/private_dot_config/wezterm/font.lua
@@ -10,7 +10,7 @@ local M = {}
 -- @param config The configuration table to modify.
 -- @param os The operating system the terminal is running in.
 M.apply = function(config, os)
-  config.font_size = os == 'windows' and 10.0 or 13.0
+  config.font_size = os == 'windows' and 9.0 or 13.0
   config.font = terminal.font_with_fallback({
     { family = 'JetBrains Mono' },
     { family = 'Symbols Nerd Font Mono', scale = 0.8 },
