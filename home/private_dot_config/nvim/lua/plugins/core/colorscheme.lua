253ba5fec9d2893ce773c7c333be005e2a175b3f
Michael Olson <michaelolson84@gmail.com>
2023-09-09 21:48:15 -0400

    More colorscheme cleanup

--- home/private_dot_config/nvim/lua/plugins/core/colorscheme.lua
+++ home/private_dot_config/nvim/lua/plugins/core/colorscheme.lua
@@ -25,11 +25,6 @@ return {
     },
   },
   {
-    'Mofiqul/vscode.nvim',
-    priority = 1000,
-    config = function() require('vscode').setup({ italic_comments = true }) end,
-  },
-  {
     'LazyVim/LazyVim',
     opts = { colorscheme = 'tokyonight' },
   },
