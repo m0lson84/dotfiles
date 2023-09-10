0b91e20c495554a2351199533d632b06bc3d4037
Michael Olson <michaelolson84@gmail.com>
2023-09-09 20:48:24 -0400

    Enable GitHub Copilot for most file types

--- home/private_dot_config/nvim/lua/plugins/coding/copilot.lua
+++ home/private_dot_config/nvim/lua/plugins/coding/copilot.lua
@@ -7,13 +7,13 @@ return {
   {
     'zbirenbaum/copilot.lua',
     keys = {
-      { '<leader>cc', '<cmd>Copilot! attach<cr>', desc = 'Enable Copilot', mode = { 'n', 'v' } },
       { '<leader>cC', '<cmd>Copilot disable<cr>', desc = 'Disable Copilot', mode = { 'n', 'v' } },
+      { '<leader>cc', '<cmd>Copilot enable<cr>', desc = 'Enable Copilot', mode = { 'n', 'v' } },
     },
     opts = {
       filetypes = {
-        markdown = true,
-        ['*'] = false,
+        dotenv = false,
+        ['*'] = true,
       },
     },
   },
