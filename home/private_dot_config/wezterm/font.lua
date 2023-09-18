7ea5cfdbd17cb178f8108362d81ee9b8fa9b272e
Michael Olson <michaelolson84@gmail.com>
2023-09-18 19:45:36 -0400

    Updating Linux specific config for Pop!_OS

--- home/private_dot_config/wezterm/font.lua
+++ home/private_dot_config/wezterm/font.lua
@@ -10,7 +10,7 @@ local M = {}
 -- @param config The configuration table to modify.
 -- @param os The operating system the terminal is running in.
 M.apply = function(config, os)
-  config.font_size = os == 'windows' and 9.0 or 12.0
+  config.font_size = os == 'darwin' and 12.0 or 9.0
   config.font = terminal.font_with_fallback({
     { family = 'JetBrains Mono' },
     { family = 'Symbols Nerd Font Mono', scale = 0.8 },
