369955342d1959f8d3c81a0aa28277af7c99e3be
Michael Olson <michael.olson@spindance.com>
2023-10-11 19:01:35 -0400

    Additional neovim keymaps for debugging

--- home/private_dot_config/nvim/lua/plugins/core/dap.lua
+++ home/private_dot_config/nvim/lua/plugins/core/dap.lua
@@ -4,4 +4,14 @@ Core debug adapter protocol configuration
 
 return {
   { import = 'lazyvim.plugins.extras.dap.core' },
+  {
+    'mfussenegger/nvim-dap',
+    keys = {
+      { '<F5>', function() require('dap').continue() end, desc = 'Continue' },
+      { '<F9>', function() require('dap').step_out() end, desc = 'Step Out' },
+      { '<F10>', function() require('dap').step_over() end, desc = 'Step Over' },
+      { '<F11>', function() require('dap').step_into() end, desc = 'Step Into' },
+      { '<F12>', function() require('dap').terminate() end, desc = 'Terminate' },
+    },
+  },
 }
