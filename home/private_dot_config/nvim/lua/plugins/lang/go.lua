95001970ce5d860d0e508150f04abccd0e78e164
Michael Olson <michaelolson84@gmail.com>
2023-10-18 00:09:53 -0400

    Simplify go configuration

--- home/private_dot_config/nvim/lua/plugins/lang/go.lua
+++ home/private_dot_config/nvim/lua/plugins/lang/go.lua
@@ -2,25 +2,12 @@
 Go language support
 --]]
 
--- Import utility functions
-local util = require('util')
-
 return {
 
   -- Add languages to treesitter
   {
     'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts) vim.treesitter.language.register('gotmpl', 'tmpl') end,
-  },
-
-  -- Configure language server
-  {
-    'neovim/nvim-lspconfig',
-    opts = {
-      servers = {
-        gopls = {},
-      },
-    },
   },
 
   -- Code annotations and documentation
