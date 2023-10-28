28377e6edb2924c42471decb32d8b4f1de2ca8b6
Michael Olson <michaelolson84@gmail.com>
2023-10-28 15:32:39 -0400

    Make unused variables/imports brighter

--- home/private_dot_config/nvim/lua/plugins/core/colorscheme.lua
+++ home/private_dot_config/nvim/lua/plugins/core/colorscheme.lua
@@ -10,12 +10,15 @@ return {
       -- styles: storm (default), night, moon, day
       style = 'night',
       on_colors = function(colors)
+        local util = require('tokyonight.util')
         colors.bg = colors.bg_dark
-        colors.border = colors.dark3
-        colors.border_highlight = colors.fg_dark
+        colors.border = util.lighten(colors.dark3, 0.95, colors.fg_dark)
+        colors.border_highlight = util.darken(colors.blue1, 0.9)
+        colors.unused = util.lighten(colors.terminal_black, 0.6, colors.fg_dark)
       end,
       on_highlights = function(highlights, colors)
         highlights.ColorColumn = { bg = colors.fg }
+        highlights.DiagnosticUnnecessary = { fg = colors.unused, bg = colors.none }
         highlights.DiagnosticVirtualTextError = { fg = colors.error, bg = colors.none }
         highlights.DiagnosticVirtualTextWarn = { fg = colors.warning, bg = colors.none }
         highlights.DiagnosticVirtualTextInfo = { fg = colors.info, bg = colors.none }
