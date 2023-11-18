179c7ccfe6b0854a942de5f28154437c9b6089ce
Michael Olson <michaelolson84@gmail.com>
2023-11-18 15:59:08 -0500

    Update treesitter parsers for go

--- home/private_dot_config/nvim/lua/plugins/lang/go.lua
+++ home/private_dot_config/nvim/lua/plugins/lang/go.lua
@@ -7,7 +7,10 @@ return {
   -- Add languages to treesitter
   {
     'nvim-treesitter/nvim-treesitter',
-    opts = function(_, opts) vim.treesitter.language.register('gotmpl', 'tmpl') end,
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed or {}, { 'templ' })
+      vim.treesitter.language.register('gotmpl', 'tmpl')
+    end,
   },
 
   -- Code annotations and documentation
