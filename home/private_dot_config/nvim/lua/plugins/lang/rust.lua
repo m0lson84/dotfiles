15d021f173de59665ccd720317ecc6f7779c2b7b
Michael Olson <michael.olson@spindance.com>
2024-03-08 08:35:35 -0500

    Removing LSP config with the move to rustaceanvim

--- home/private_dot_config/nvim/lua/plugins/lang/rust.lua
+++ home/private_dot_config/nvim/lua/plugins/lang/rust.lua
@@ -4,16 +4,6 @@ Rust language support
 
 return {
 
-  -- Configure language server
-  {
-    'neovim/nvim-lspconfig',
-    opts = {
-      servers = {
-        rust_analyzer = {},
-      },
-    },
-  },
-
   -- Code annotations and documentation
   {
     'danymat/neogen',
