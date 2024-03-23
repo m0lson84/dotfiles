fe8debe739e96d890ffb5e1bfa9a1f46a4ee194f
Michael Olson <michaelolson84@gmail.com>
2024-03-22 20:05:52 -0400

    Updates for rustaceanvin

--- home/private_dot_config/nvim/lua/plugins/lang/rust.lua
+++ home/private_dot_config/nvim/lua/plugins/lang/rust.lua
@@ -4,6 +4,12 @@ Rust language support
 
 return {
 
+  -- Configure language server
+  {
+    'neovim/nvim-lspconfig',
+    opts = function(_, opts) opts.servers.rust_analyzer = nil end,
+  },
+
   -- Code annotations and documentation
   {
     'danymat/neogen',
