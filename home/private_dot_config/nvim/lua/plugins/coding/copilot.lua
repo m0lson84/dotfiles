0c088093c525ab02b4376a02d6ffaad185e94802
Michael Olson <michaelolson84@gmail.com>
2024-04-02 23:04:13 -0400

    Removing keymaps for Copilot

--- home/private_dot_config/nvim/lua/plugins/coding/copilot.lua
+++ home/private_dot_config/nvim/lua/plugins/coding/copilot.lua
@@ -5,10 +5,6 @@ GitHub Copilot (https://github.com/zbirenbaum/copilot.lua)
 return {
   {
     'zbirenbaum/copilot.lua',
-    keys = {
-      { '<leader>cC', '<cmd>Copilot disable<cr>', desc = 'Disable Copilot', mode = { 'n', 'v' } },
-      { '<leader>cc', '<cmd>Copilot enable<cr>', desc = 'Enable Copilot', mode = { 'n', 'v' } },
-    },
     opts = {
       filetypes = {
         dotenv = false,
