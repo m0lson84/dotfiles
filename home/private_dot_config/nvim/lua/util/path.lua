6ba3fdb28b73204c52a3783961cfb33e1e4110ee
Michael Olson <michaelolson84@gmail.com>
2024-05-08 22:09:52 -0400

    Removing deprecated nvim apis

--- home/private_dot_config/nvim/lua/util/path.lua
+++ home/private_dot_config/nvim/lua/util/path.lua
@@ -9,6 +9,6 @@ local M = {}
 --- Join a list of paths.
 ---@param ... string | string[] The paths to join.
 ---@return string path The joined path.
-M.join = function(...) return table.concat(vim.tbl_flatten({ ... }), '/') end
+M.join = function(...) return table.concat(vim.iter({ ... }):flatten():totable(), '/') end
 
 return M
