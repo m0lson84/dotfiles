5f695298d31fd279b2104ea766db4a3271e46ad6
Michael Olson <michael.olson@spindance.com>
2024-05-13 07:32:00 -0400

    Enable flash.nvim by default

---
+++ home/private_dot_config/nvim/lua/plugins/editor/flash.lua
@@ -0,0 +1,12 @@
+--[[
+flash.nvim (https://github.com/folke/flash.nvim)
+--]]
+
+return {
+  {
+    'folke/flash.nvim',
+    opts = {
+      enabled = true,
+    },
+  },
+}
--- home/private_dot_config/nvim/lua/plugins/editor/init.lua
+++ home/private_dot_config/nvim/lua/plugins/editor/init.lua
@@ -3,6 +3,7 @@ Additional editor plugins
 --]]
 
 return {
+  require('plugins.editor.flash'),
   require('plugins.editor.folding'),
   require('plugins.editor.ruler'),
 }
