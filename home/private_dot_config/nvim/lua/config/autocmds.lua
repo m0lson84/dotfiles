19238d4f39b8fa86561e5e37c929072a52b557c2
Michael Olson <michaelolson84@gmail.com>
2024-05-28 21:07:42 -0400

    Quit Zellij when exiting Neovim

--- home/private_dot_config/nvim/lua/config/autocmds.lua
+++ home/private_dot_config/nvim/lua/config/autocmds.lua
@@ -10,6 +10,13 @@ Core LazyVim auto commands
 ---@param name string The name of the group to create
 local function create_group(name) return vim.api.nvim_create_augroup('local_' .. name, { clear = true }) end
 
+-- Quit Zellij when quitting Neovim
+vim.api.nvim_create_autocmd({ 'VimLeave' }, {
+  group = create_group('quit_zellij'),
+  pattern = '*',
+  callback = function() os.execute('zellij k "$ZELLIJ_SESSION_NAME"') end,
+})
+
 -- Configure language ruler
 vim.api.nvim_create_autocmd({ 'FileType' }, {
   group = create_group('language_ruler'),
