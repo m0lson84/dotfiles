9ef74c559cce866951795198206ec621afe77646
Michael Olson <michael.olson@spindance.com>
2024-06-17 16:38:23 -0400

    Adding alacritty configuration

--- home/.chezmoiscripts/windows/run_after_apply-config.ps1
+++ home/.chezmoiscripts/windows/run_after_apply-config.ps1
@@ -27,6 +27,10 @@ function Apply-Settings() {
 # Stop script on error
 $ErrorActionPreference = 'Stop'
 
+# Apply Alacritty settings
+Write-Host "Applying Alacritty settings..."
+Apply-Settings -Source "$env:USERPROFILE/.config/alacritty/*" -Target "$env:APPDATA/alacritty"
+
 # Apply Powershell profile
 Write-Host "Applying Powershell profile..."
 Apply-Settings -Source "$env:USERPROFILE/.config/powershell/Profile.ps1" -Target "$env:USERPROFILE/Documents/PowerShell"
