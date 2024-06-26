d6234475b1e58ecd97722135533877348a61dfc4
Michael Olson <michaelolson84@gmail.com>
2024-06-25 22:31:01 -0400

    Additional keymaps for csharp lsp

--- home/private_dot_config/nvim/lua/plugins/lang/csharp.lua
+++ home/private_dot_config/nvim/lua/plugins/lang/csharp.lua
@@ -2,6 +2,18 @@
 C# language support
 --]]
 
+--- Find all references handler
+local find_all_references = function() require('omnisharp_extended').telescope_lsp_references() end
+
+--- Go to definition handler
+local goto_definition = function() require('omnisharp_extended').telescope_lsp_definitions() end
+
+--- Go to implementation handler
+local goto_implementation = function() require('omnisharp_extended').telescope_lsp_implementation() end
+
+--- Go to type definition handler
+local goto_type_definition = function() require('omnisharp_extended').telescope_lsp_type_definitions() end
+
 return {
 
   -- Add languages to treesitter
@@ -19,13 +31,15 @@ return {
         omnisharp = {
           handlers = {
             ['textDocument/definition'] = function(...) return require('omnisharp_extended').handler(...) end,
+            ['textDocument/typeDefinition'] = function(...) return require('omnisharp_extended').handler(...) end,
+            ['textDocument/references'] = function(...) return require('omnisharp_extended').handler(...) end,
+            ['textDocument/implementation'] = function(...) return require('omnisharp_extended').handler(...) end,
           },
           keys = {
-            {
-              'gd',
-              function() require('omnisharp_extended').telescope_lsp_definitions() end,
-              desc = 'Goto Definition',
-            },
+            { 'gd', goto_definition, desc = 'Goto Definition' },
+            { 'gi', goto_implementation, desc = 'Goto Implementation' },
+            { 'gr', find_all_references, desc = 'Find All References' },
+            { '<leader>D', goto_type_definition, desc = 'Goto Type Definition' },
           },
           enable_roslyn_analyzers = true,
           organize_imports_on_format = true,
