18a0a262316d22c76efed07030e95919c9b8ddcb
Michael Olson <michael.olson@spindance.com>
2024-08-22 09:02:53 -0400

    Use neovim as git diff and merge tool

---
+++ home/private_dot_config/nvim/lua/plugins/util/diffview.lua
@@ -0,0 +1,13 @@
+--[[
+-- Diffview.nvim (https://github.com/sindrets/diffview.nvim)
+--]]
+
+return {
+  {
+    'sindrets/diffview.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      enhanced_diff_hl = true,
+    },
+  },
+}
