e99cc35805f25a50000aa4f08c812b06a851d5aa
Michael Olson <michaelolson84@gmail.com>
2024-12-10 10:07:34 -0500

    Rendering options for WezTerm

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -18,19 +18,22 @@ if terminal.config_builder then config = terminal.config_builder() end
 -- General
 config.check_for_updates = false
 config.enable_kitty_keyboard = true
-config.front_end = 'WebGpu'
-config.max_fps = 240
 config.term = 'wezterm'
 
--- Window
-config.window_close_confirmation = 'NeverPrompt'
-config.window_decorations = 'TITLE | RESIZE | MACOS_FORCE_DISABLE_SHADOW'
+-- Rendering
+config.enable_wayland = true
+config.front_end = 'WebGpu'
+config.max_fps = 240
 
 -- Tab bar
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
+
+-- Window
+config.window_close_confirmation = 'NeverPrompt'
+config.window_decorations = 'TITLE | RESIZE | MACOS_FORCE_DISABLE_SHADOW'
 
 -- Start in WSL if running on windows
 if util.get_target().platform == 'windows' then config.default_prog = { 'ubuntu' } end
